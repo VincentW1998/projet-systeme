24edc3a5f1d69152a52d734ea7066f6870e08533
ngof <felix.ngo@etu.univ-paris-diderot.fr>
2020-10-07 10:56:17 +0200

    fix error in ls

--- code/my_ls.c
+++ code/my_ls.c
@@ -7,7 +7,7 @@ int ls(char buff[], DIR * current){
   char * token = strtok(buff," ");//decoupe buff a partir de " "
   token = strtok(NULL," ");
   while((directory = readdir(current))!=NULL) //affecte a directory un des fichiers de current
-    if(token != NULL && !strcmp(token,"-a\n"))//check si l'usr utilise l'option -a de ls
+    if(token != NULL && !strncmp(token,"-a\n",3))//check si l'usr utilise l'option -a de ls
       write(1,strcat(directory->d_name,"  "),strlen(directory->d_name)+1);//ecrit dans la sortie standard le nom du fichier
     else
       if(!strncmp(buff,"ls\n",strlen("ls\n")) && directory->d_name[0]!='.')//omet les fichiers commencant par un .
