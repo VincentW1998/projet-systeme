e329ecbdbb0514572a86ebfcb0d279e2d455d248
Vincent Wei <vincent.wei@etu.univ-paris-diderot.fr>
2020-10-17 01:37:05 +0200

    ajout de la fonction rmdir dans le shell

--- code/shell.c
+++ code/shell.c
@@ -1,12 +1,11 @@
 #define BUFSIZE 512
 #include <stdlib.h>
 #include "my_ls.h"
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 #include <dirent.h>
 #include "my_mkdir.h"
+#include "my_rmdir.h"
 
 int main(int argc, char const *argv[]) {
   char buff [BUFSIZE];
@@ -14,8 +13,8 @@ int main(int argc, char const *argv[]) {
   current = opendir(".");
   int n, file, lu;
   char * token;
-  while(1){ // boucle infinie
-    if((n = read(0,buff,BUFSIZE)) > 0){ // check si l'usr ecrit dans l'entrée
+  while(1) { // boucle infinie
+    if((n = read(0,buff,BUFSIZE)) > 0) { // check si l'usr ecrit dans l'entrée
       // implementation de la commande exit
 
       if(!strncmp(buff,"exit\n",strlen("exit\n")))
@@ -23,12 +22,18 @@ int main(int argc, char const *argv[]) {
       // implementation ls
       if(!strncmp(buff,"ls",strlen("ls")))
         ls(buff, current);
-      if(!strncmp(buff, "mkdir", strlen("mkdir"))){
+      if(!strncmp(buff, "mkdir", strlen("mkdir"))) {
 
         token = strtok(buff, " \n");
         token = strtok(NULL, " \n");
 
         my_mkdir(token);
+      }
+      if(!strncmp(buff, "rmdir", strlen("rmdir"))) {
+        token = strtok(buff, " \n");
+        token = strtok(NULL, " \n");
+
+        my_rmdir(token);
       }
     }
   }
