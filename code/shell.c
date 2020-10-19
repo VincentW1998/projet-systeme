9158975d97cd818afd50db8b9ae5e81967a95c17
Vincent Wei <vincent.wei@etu.univ-paris-diderot.fr>
2020-10-19 19:27:12 +0200

    factorisation du main en fonctions auxiliaires

--- code/shell.c
+++ code/shell.c
@@ -11,7 +11,7 @@
 // #include "my_mkdir.h"
 // #include "my_rmdir.h"
 
-int affichagePrompt() {
+int affichagePrompt() { // affichage du prompt
   write(1, KBLU, strlen(KBLU));
   write(1, getcwd(NULL, 0), strlen(getcwd(NULL, 0)));
   write(1, "> ", 2);
@@ -19,83 +19,100 @@ int affichagePrompt() {
   return 0;
 }
 
+
+int execCommand(char ** command) {
+  pid_t pid = fork();
+  if(pid != 0) { // on attend que le processus fils se lance
+    wait(NULL);
+  }
+  else { // puis on execute la commande souhaité
+    execvp(command[0], command);
+    return 0;
+  }
+  return -1;
+}
+
+void *lectureLigne(char * token, char * str, char * buff){ //readLine
+  token = strtok(str,"\n");
+  if(token!=NULL){
+    buff = malloc(strlen(token) + 1);
+    strcpy(buff,token);
+    strcat(buff,"\n");
+  }
+  return buff;
+}
+
+int separateurCommand(char * token, char * buff, char ** command){ // separe la ligne en tableau de char
+  token = strtok(buff, " \n");
+  command[0] = malloc(strlen(token) + 1);
+  strcpy(command[0], token);
+  int i = 1;
+
+  while((token = strtok(NULL, " \n")) != NULL) {
+    command[i] = malloc(strlen(token) + 1);
+    strcpy(command[i], token);
+    i ++;
+  }
+  int nbOption = i;
+  command[i] = NULL;
+  command[i+1] = NULL;
+  return nbOption;
+}
+
+int commandPersonnalisee(char ** command) {
+  int nbCommand = 3;
+  char * commandPerso[nbCommand];
+  int numeroCommand = -1;
+  commandPerso[0] = "exit";
+  commandPerso[1] = "cd";
+  commandPerso[2] = "pwd";
+
+  for (size_t i = 0; i < nbCommand; i++) {
+    if(!strcmp(commandPerso[i], command[0]))
+    numeroCommand = i;
+  }
+  switch (numeroCommand) {
+    case -1 : return -1;
+    case 0 : exit(0);
+    case 1 :
+      chdir(command[1]);
+      break;
+    default :
+    write(1, getcwd(NULL, 0), strlen(getcwd(NULL, 0)));
+    write(1, "\n\n", 2);
+    break;
+  }
+  return 0;
+}
+
+
+
 int main(int argc, char const *argv[]) {
   char str [BUFSIZE];
   char * buff, * buff2, * token;
   int n, i = 1;
   char * command[100];
   int nbOption = 0;
-
+  int choix;
 
   while(1) { // boucle infinie
     affichagePrompt();
     nbOption = 0;
     if((n = read(0,str,BUFSIZE) > 0)) { // check si l'usr ecrit dans l'entrée
 
-      token = strtok(str,"\n");
-      if(token!=NULL){
-        buff = malloc(strlen(token) + 1);
-        strcpy(buff,token);
-        strcat(buff,"\n");
-      }
+      buff = lectureLigne(token, str, buff);
 
       // creation de buff2 car buff va etre 'ecraser' par strtok
       buff2 = malloc(strlen(buff) + 1);
       strcpy(buff2, buff);
 
       // separe buff en command, option, path
-      token = strtok(buff, " \n");
-      command[0] = malloc(strlen(token) + 1);
-      strcpy(command[0], token);
-      i = 1;
-      while((token = strtok(NULL, " \n")) != NULL) {
-        command[i] = malloc(strlen(token) + 1);
-        strcpy(command[i], token);
-        i ++;
+      nbOption = separateurCommand(token, buff, command);
+      choix = commandPersonnalisee(command);
+      if(choix == -1){
+        execCommand(command);
       }
 
-    nbOption = i;
-     command[i] = NULL;
-     command[i+1] = NULL;
-
-     pid_t pid = fork();
-     if(pid != 0) { // on attend que le processus fils se lance
-       wait(NULL);
-     }
-     else { // puis on execute la commande souhaité
-       execvp(command[0], command);
-     }
-
-      if(!strncmp(buff2,"exit\n",strlen("exit\n")))
-        break;
-      // implementation ls
-      // if(!strncmp(buff,"ls",strlen("ls")))
-      //   ls(buff);
-      //
-      // if(!strncmp(buff, "mkdir", strlen("mkdir"))) {
-      //
-      //   token = strtok(buff, " \n");
-      //   token = strtok(NULL, " \n");
-      //
-      //   my_mkdir(token);
-      // }
-      // if(!strncmp(buff, "rmdir", strlen("rmdir"))) {
-      //   token = strtok(buff, " \n");
-      //   token = strtok(NULL, " \n");
-      //
-      //
-      //   my_rmdir(token);
-      // }
-      if(!strncmp(buff2, "cd", strlen("cd"))) {
-        token = strtok(buff2, " \n");
-        token = strtok(NULL, " \n");
-
-        chdir(token);
-      }
-      if(!strncmp(buff2, "pwd", strlen("pwd"))) {
-        write(1, getcwd(NULL, 0), strlen(getcwd(NULL, 0)));
-        write(1, "\n\n", 2);
-      }
       for (size_t j = 0; j < nbOption; j++) {
         command[j] = NULL;
       }
