ea7b19d1c8e268ee34aed7f54fa9afd96966039b
Vincent Wei <vincent.wei@etu.univ-paris-diderot.fr>
2020-10-19 06:11:02 +0200

    utilisation de execvp

--- code/shell.c
+++ code/shell.c
@@ -2,25 +2,32 @@
 #define KBLU  "\x1B[34m"
 #define RESET "\x1B[0m"
 #include <stdlib.h>
-#include "my_ls.h"
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 #include <dirent.h>
-#include "my_mkdir.h"
-#include "my_rmdir.h"
+#include <sys/wait.h>
+// #include "my_ls.h"
+// #include "my_mkdir.h"
+// #include "my_rmdir.h"
+
 
 int affichagePrompt() {
   write(1, KBLU, strlen(KBLU));
   write(1, getcwd(NULL, 0), strlen(getcwd(NULL, 0)));
   write(1, "> ", 2);
   write(1, RESET, strlen(RESET));
+  return 0;
 }
 
 int main(int argc, char const *argv[]) {
   char str [BUFSIZE];
-  char * buff;
+  char ** test;
+  char * buff, * buff2, * token;
   int n;
-  char * token;
+  int i = 1;
+  char * command[100];
+
   while(1) { // boucle infinie
     affichagePrompt();
     if((n = read(0,str,BUFSIZE) > 0)) { // check si l'usr ecrit dans l'entrée
@@ -31,38 +38,59 @@ int main(int argc, char const *argv[]) {
         strcat(buff,"\n");
       }
 
-      if(!strncmp(buff,"exit\n",strlen("exit\n")))
+      buff2 = malloc(strlen(buff) + 1);
+      strcpy(buff2, buff);
+
+      token = strtok(buff, " \n");
+      strcpy(command[0], token);
+      while((token = strtok(NULL, " \n")) != NULL) {
+        command[i] = malloc(strlen(token) + 1);
+        strcpy(command[i], token);
+        i ++;
+      }
+
+     command[i] = NULL;
+     command[i+1] = NULL;
+     pid_t pid = fork();
+     if(pid != 0) {
+       wait(NULL);
+     }
+     else {
+       execvp(command[0], command);
+     }
+
+      if(!strncmp(buff2,"exit\n",strlen("exit\n")))
         break;
       // implementation ls
-      if(!strncmp(buff,"ls",strlen("ls")))
-        ls(buff);
-
-      if(!strncmp(buff, "mkdir", strlen("mkdir"))) {
-
-        token = strtok(buff, " \n");
-        token = strtok(NULL, " \n");
-
-        my_mkdir(token);
-      }
-      if(!strncmp(buff, "rmdir", strlen("rmdir"))) {
-        token = strtok(buff, " \n");
-        token = strtok(NULL, " \n");
-
-
-        my_rmdir(token);
-      }
-      if(!strncmp(buff, "cd", strlen("cd"))) {
-        token = strtok(buff, " \n");
-        token = strtok(NULL, " \n");
-
-        chdir(token);
-      }
-      if(!strncmp(buff, "pwd", strlen("pwd"))) {
-        write(1, getcwd(NULL, 0), strlen(getcwd(NULL, 0)));
-        write(1, "\n\n", 2);
-      }
+      // if(!strncmp(buff,"ls",strlen("ls")))
+      //   ls(buff);
+      //
+      // if(!strncmp(buff, "mkdir", strlen("mkdir"))) {
+      //
+      //   token = strtok(buff, " \n");
+      //   token = strtok(NULL, " \n");
+      //
+      //   my_mkdir(token);
+      // }
+      // if(!strncmp(buff, "rmdir", strlen("rmdir"))) {
+      //   token = strtok(buff, " \n");
+      //   token = strtok(NULL, " \n");
+      //
+      //
+      //   my_rmdir(token);
+      // }
+      // if(!strncmp(buff2, "cd\n", strlen("cd\n"))) {
+      //   token = strtok(buff2, " \n");
+      //   token = strtok(NULL, " \n");
+      //
+      //   chdir(token);
+      // }
+      // if(!strncmp(buff2, "pwd\n", strlen("pwd\n"))) {
+      //   write(1, getcwd(NULL, 0), strlen(getcwd(NULL, 0)));
+      //   write(1, "\n\n", 2);
+      // }
     }
-    strcpy(buff,"");
+    strcpy(buff2,"");
   }
   return 0;
 }
