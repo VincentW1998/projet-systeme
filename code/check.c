94380d7ff345bc0ddc536a0ab1c49116e420ad5f
Vincent Wei <vincent.wei@etu.univ-paris-diderot.fr>
2020-12-20 22:39:44 +0100

    new read, check for mkdir

--- code/check.c
+++ code/check.c
@@ -1,6 +1,5 @@
-#include "check.h"
 #include "tar.h"
-
+#include "check.h"
 
 
 int found;
@@ -34,12 +33,54 @@ int read_header(int fd, char *path) {
   return filesize;
 }
 
+int read_header2(int fd, char *path, struct posix_header * newHd) {
+  struct posix_header hd;
+  unsigned int filesize = 0;
+  int nb = read(fd, &hd, BLOCKSIZE);
+  if(nb == -1) {
+    perror("read");
+    return -1;
+  }
+  found = 0;
+  printf("lseek : %ld\n", lseek(fd, 0, SEEK_CUR));
+
+  if(hd.name[0] == '\0') {
+    lseek(fd, -512, SEEK_CUR);
+    char blockEnd[BLOCKSIZE];
+    memset(blockEnd, '\0', BLOCKSIZE);
+    write(fd, newHd, BLOCKSIZE);
+    write(fd, blockEnd, BLOCKSIZE);
+
+    return -1;
+  }  printf("name : %s\n", hd.name);
+  if(strcmp(hd.name, path) == 0) found = 1;
+  sscanf(hd.size, "%o", &filesize);
+  return filesize;
+}
+
 int checkEntete(char * tarName, char * path) {
   int fd;
   int filesize = 0;
   fd = open(tarName, O_RDONLY);
   while(1) {
     filesize = read_header(fd, path);
+    if (filesize == -1) break;
+    if (found) {
+      close(fd);
+      return 1;
+    }
+    next_header(fd, filesize);
+  }
+  close (fd);
+  return -1;
+}
+
+int checkEntete2(char * tarName, char * path, struct posix_header * hd) {
+  int fd;
+  int filesize = 0;
+  fd = open(tarName, O_RDWR);
+  while(1) {
+    filesize = read_header2(fd, path, hd);
     if (filesize == -1) break;
     if (found) {
       close(fd);
