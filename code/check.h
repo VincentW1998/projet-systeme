85da384232e5ae58673fef587d1d9fe2146fae1f
Vincent Wei <vincent.wei@etu.univ-paris-diderot.fr>
2020-12-20 19:10:47 +0100

    add include in check.h

--- code/check.h
+++ code/check.h
@@ -1,5 +1,13 @@
 #ifndef CHECK_H
 #define CHECK_H
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <grp.h>
+#include <pwd.h>
+#include <fcntl.h>
 
 int found;
 char buf[512];
