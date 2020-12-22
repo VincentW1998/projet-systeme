e018cb9879ad7c2b140544db7521f44683547179
Julien BERSAUTER <julien.bersauter@orange.fr>
2020-12-22 10:33:36 +0100

    Fonction «cpfichier_intratar» : Copie d'un fichier depuis et vers le même tar.

---
+++ code/moncp.c
@@ -0,0 +1,43 @@
+#include "util_tar.h"
+#include "tar.c"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+int cpfichier_intratar(int fichier,char *fic,char *c){
+  //Copie à la fin du tar un fichier déjà présent.
+  fin(fichier);
+  off_t decalage = lseek(fichier,0,SEEK_CUR);
+  
+  rechercher(fichier,1,fic);
+  
+  struct posix_header entete;
+  ssize_t lect = read(fichier,&entete,512);
+  
+  strncpy(entete.name,c,100);
+  set_checksum(&entete);
+  
+  pwrite(fichier,&entete,512,decalage);
+  
+  decalage+=512;
+  char *tampon [512];
+  
+  char *ctaille = entete.size;
+  int taille;
+  int sc = sscanf(ctaille,"%o",&taille);
+  int nb = ((taille+512-1)/512);
+
+  for (int i = 0; i<nb;i++){
+    read(fichier,&tampon,512);
+    pwrite(fichier,tampon,512,decalage);
+    decalage+=512;
+  }
+  char *tnull[1024];
+  //correspond aux deux blocs finaux de taille 512 de l'archive tar.
+  memset(tnull,(int)'\0',1024);
+  pwrite(fichier,tnull,1024,decalage);
+  return 0;
+}
