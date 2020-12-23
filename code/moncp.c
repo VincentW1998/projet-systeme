73c7c7b512e617dd485df648dddad96c911e719e
Julien BERSAUTER <julien.bersauter@orange.fr>
2020-12-23 12:17:00 +0100

    cp -r version 1 (pas encore testée)

--- code/moncp.c
+++ code/moncp.c
@@ -1,5 +1,5 @@
 #include "util_tar.h"
-#include "tar.c"
+#include "monrm.c"
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -42,8 +42,21 @@ int cpfichier_intratar(int fichier,char *fic,char *c){
   return 0;
 }
 
-int cpfichier_intertar(int fichier1, int fichier2, char *fic, char *c){
+int clear(int fichier, char *dos, char *archive ,char *c){
+  if(rechercher(fichier,1,c)){
+    struct posix_header entete;
+    ssize_t lect =read(fichier,&entete,512);
+    if(entete.typeflag!='5'){
+      return rmfichier_tar(fichier,dos,archive,c);
+    }
+  }
+  else return -1;
+}
+
+int cpfichier_intertar(int fichier1, int fichier2, char *dosarc2, char *arc2, char *fic, char *c){
   rechercher(fichier1,1,fic);
+  fichier2 = clear(fichier2, dosarc2, arc2,c);
+  //clear va supprimer le fichier qui va être écrasé.
   fin(fichier2);
 
   struct posix_header entete;
@@ -67,4 +80,37 @@ int cpfichier_intertar(int fichier1, int fichier2, char *fic, char *c){
   char *tnull[1024];
   memset(tnull,(int)'\0',1024);
   write(fichier2,tnull,1024);
+}
+
+int cp_r_intertar(int fichier1, int fichier2, char *dosarc2, char *arc2, char *fic, char *c){
+  //copie récursive dossier
+  struct posix_header entete;
+  if(rechercher(fichier1,0,fic)==1){
+    int chm = strlen(fic);
+    char *ctaille = entete.size;
+    int taille;
+    int sc = sscanf(ctaille,"%o",&taille);
+    int nb = ((taille+512-1)/512);
+    char tab[500];
+    
+    int v = copier(fic,tab,0);
+    
+    while(read(fichier1,&entete,512)>0){
+       ctaille = entete.size;
+       taille;
+       sc = sscanf(ctaille,"%o",&taille);
+       nb = ((taille+512-1)/512);
+       if(strncmp(entete.name,fic,chm)==0){
+	 copier(entete.name,tab,v);
+	 cpfichier_intertar(fichier1, fichier2, dosarc2, arc2,entete.name, fic);
+	 cleartab(v, tab);
+       }
+       else lseek(fichier1,nb*512,SEEK_CUR);
+    }
+    return 0;
+  }
+}
+
+int cp_r_intratar(){
+  return 0;
 }
