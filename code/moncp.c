13c69244aaf45cb8e721efb7a236649454a10bdf
Julien BERSAUTER <julien.bersauter@orange.fr>
2020-12-23 15:51:22 +0100

    Améliorations du code et correction de bugs. C'est presque fini.

--- code/moncp.c
+++ code/moncp.c
@@ -47,25 +47,25 @@ int clear(int fichier, char *dos, char *archive ,char *c){
     struct posix_header entete;
     ssize_t lect =read(fichier,&entete,512);
     if(entete.typeflag != '5'){
-      printf("Ici, nous supprimons.\n");
       return rmfichier_tar(fichier,dos,archive,c);
     }
     return fichier;
   }
-  else return -1;
+  else return fichier;
     
 }
 
-int cpfichier_intertar(int fichier1, int fichier2, char *dosarc2, char *arc2, char *fic, char *c){
-  rechercher(fichier1,1,fic);
-  fichier2 = clear(fichier2, dosarc2, arc2,c);
+int cpfichier_intertar
+(int fichier1, int fichier2, char *dossierarchive,char *cheminarchive, char *cible, char * dest){
+  rechercher(fichier1,1,cible);
+  fichier2 = clear(fichier2,dossierarchive, cheminarchive,dest);
   //clear va supprimer le fichier qui va être écrasé.
   fin(fichier2);
 
   struct posix_header entete;
   ssize_t lect = read(fichier1,&entete,512);
 
-  strncpy(entete.name,c,100);
+  strncpy(entete.name,dest,100);
   set_checksum(&entete);
 
   write(fichier2,&entete,512);
@@ -83,6 +83,7 @@ int cpfichier_intertar(int fichier1, int fichier2, char *dosarc2, char *arc2, ch
   char *tnull[1024];
   memset(tnull,(int)'\0',1024);
   write(fichier2,tnull,1024);
+  return fichier2;
 }
 
 int cp_r_intertar(int fichier1, int fichier2, char *dosarc2, char *arc2, char *fic, char *c){
@@ -95,7 +96,7 @@ int cp_r_intertar(int fichier1, int fichier2, char *dosarc2, char *arc2, char *f
     int sc = sscanf(ctaille,"%o",&taille);
     int nb = ((taille+512-1)/512);
     char tab[500];
-    //int v = copier(fic,tab,0);
+    
     int i=0;
     while(i<500 && i<strlen(c) && c[i]!='\0'){
       tab[i]=c[i];
@@ -103,40 +104,36 @@ int cp_r_intertar(int fichier1, int fichier2, char *dosarc2, char *arc2, char *f
     }
     int j=i;
     int k;
-    printf("fic %s\n",fic);printf("tab = %s\n", tab);
-    printf("taille tab = %d\n",sizeof(tab));
-    
+
     while(read(fichier1,&entete,512)>0){
       ctaille = entete.size;
       taille;
       sc = sscanf(ctaille,"%o",&taille);
       nb = ((taille+512-1)/512);
-       if(strncmp(entete.name,fic,chm)==0){
-	 //copier(entete.name,tab,v);
-	 k=0;
-	 while((k+j)<500 && k<strlen(entete.name)){
-	   printf("entete.name sizeof : %s %d\n",entete.name,strlen(entete.name));
-	   tab[k+j]=entete.name[k];
-	   k++;
-	 }
-	 printf("nom du fichier copié : %s\n.",tab);
-	 
-	 cpfichier_intertar(fichier1, fichier2, dosarc2, arc2,entete.name, tab);
-	 suivant(fichier1);
-	 //cleartab(v, tab);
-	 memset(tab,'\0',500);
-	 
-	 i=0;
-	 while(i<500 && i<strlen(c)){
-	   tab[i]=c[i];
-	   i++;
-	 }
-	 
-       }
-       else lseek(fichier1,nb*512,SEEK_CUR);
+
+      if(strncmp(entete.name,fic,chm)!=0)
+	lseek(fichier1,nb*512,SEEK_CUR);
+      else {
+	k=0;
+	while((k+j)<500 && k<strlen(entete.name)){
+	  tab[k+j]=entete.name[k];
+	  k++;
+	}
+	int parcours = lseek(fichier1,0,SEEK_CUR);
+	fichier2 = cpfichier_intertar(fichier1, fichier2, dosarc2, arc2,entete.name, tab);
+	lseek(fichier1,parcours,SEEK_SET);
+	lseek(fichier1,nb*512,SEEK_CUR);
+	memset(tab,'\0',500);
+	i=0;
+	while(i<500 && i<strlen(c)){
+	  tab[i]=c[i];
+	  i++;
+	}
+      }
     }
     return 0;
   }
+  return 0;
 }
 
 int cp_r_intratar(){
