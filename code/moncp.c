bbed1bd12b3960021f7abc91f7c763b058ecda14
Julien BERSAUTER <julien.bersauter@orange.fr>
2020-12-23 14:21:28 +0100

    cp suite (quelques bugs)

--- code/moncp.c
+++ code/moncp.c
@@ -46,11 +46,14 @@ int clear(int fichier, char *dos, char *archive ,char *c){
   if(rechercher(fichier,1,c)){
     struct posix_header entete;
     ssize_t lect =read(fichier,&entete,512);
-    if(entete.typeflag!='5'){
+    if(entete.typeflag != '5'){
+      printf("Ici, nous supprimons.\n");
       return rmfichier_tar(fichier,dos,archive,c);
     }
+    return fichier;
   }
   else return -1;
+    
 }
 
 int cpfichier_intertar(int fichier1, int fichier2, char *dosarc2, char *arc2, char *fic, char *c){
@@ -92,18 +95,43 @@ int cp_r_intertar(int fichier1, int fichier2, char *dosarc2, char *arc2, char *f
     int sc = sscanf(ctaille,"%o",&taille);
     int nb = ((taille+512-1)/512);
     char tab[500];
-    
-    int v = copier(fic,tab,0);
+    //int v = copier(fic,tab,0);
+    int i=0;
+    while(i<500 && i<strlen(c) && c[i]!='\0'){
+      tab[i]=c[i];
+      i++;
+    }
+    int j=i;
+    int k;
+    printf("fic %s\n",fic);printf("tab = %s\n", tab);
+    printf("taille tab = %d\n",sizeof(tab));
     
     while(read(fichier1,&entete,512)>0){
-       ctaille = entete.size;
-       taille;
-       sc = sscanf(ctaille,"%o",&taille);
-       nb = ((taille+512-1)/512);
+      ctaille = entete.size;
+      taille;
+      sc = sscanf(ctaille,"%o",&taille);
+      nb = ((taille+512-1)/512);
        if(strncmp(entete.name,fic,chm)==0){
-	 copier(entete.name,tab,v);
-	 cpfichier_intertar(fichier1, fichier2, dosarc2, arc2,entete.name, fic);
-	 cleartab(v, tab);
+	 //copier(entete.name,tab,v);
+	 k=0;
+	 while((k+j)<500 && k<strlen(entete.name)){
+	   printf("entete.name sizeof : %s %d\n",entete.name,strlen(entete.name));
+	   tab[k+j]=entete.name[k];
+	   k++;
+	 }
+	 printf("nom du fichier copié : %s\n.",tab);
+	 
+	 cpfichier_intertar(fichier1, fichier2, dosarc2, arc2,entete.name, tab);
+	 suivant(fichier1);
+	 //cleartab(v, tab);
+	 memset(tab,'\0',500);
+	 
+	 i=0;
+	 while(i<500 && i<strlen(c)){
+	   tab[i]=c[i];
+	   i++;
+	 }
+	 
        }
        else lseek(fichier1,nb*512,SEEK_CUR);
     }
@@ -113,4 +141,19 @@ int cp_r_intertar(int fichier1, int fichier2, char *dosarc2, char *arc2, char *f
 
 int cp_r_intratar(){
   return 0;
+}
+
+int main(){
+  int f = open("a.tar",O_RDWR);
+  int g = open("b.tar",O_RDWR);
+  if(f<=0){
+    perror("( f )");
+  }
+
+  if (g<=0){
+    perror("( g )");
+  }
+  
+  
+  cp_r_intertar(g,f,"./","./b.tar","b/","a/");
 }
