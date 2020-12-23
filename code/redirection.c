893b48a8801648b40064a7859473d7a3e950c11f
ngof <felix.ngo@etu.univ-paris-diderot.fr>
2020-12-24 00:35:48 +0100

    add redirection files

---
+++ code/redirection.c
@@ -0,0 +1,8 @@
+//
+//  redirection.c
+//  shell
+//
+//  Created by Félix Ngo on 24/12/2020.
+//
+
+#include "redirection.h"
---
+++ code/redirection.h
@@ -0,0 +1,13 @@
+//
+//  redirection.h
+//  shell
+//
+//  Created by Félix Ngo on 24/12/2020.
+//
+
+#ifndef redirection_h
+#define redirection_h
+
+#include <stdio.h>
+
+#endif /* redirection_h */
