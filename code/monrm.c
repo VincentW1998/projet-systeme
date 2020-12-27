c63f436b26ddb2cff83961db202b44c86ff93af2
Julien BERSAUTER <julien.bersauter@orange.fr>
2020-12-27 11:31:46 +0100

    Correction d'un bug de suppression dans rm et rm -r

--- code/monrm.c
+++ code/monrm.c
@@ -6,6 +6,92 @@
 #include "check.h"
 #include "myCd.h"
 
+static int debut3(int fichier){
+  return lseek(fichier,0,SEEK_SET);
+}
+
+static int suivant3(int fichier){
+  struct posix_header entete;
+  ssize_t lect = read(fichier,&entete,512);
+
+  if(lect==0) return -1;
+  
+  if(lect<0){
+    perror("Echec de la lecture dans suivant ");
+    return -1;
+  }
+
+  if(entete.name[0]=='\0'){
+    //printf("cas ?\n");
+    lseek(fichier,-512,SEEK_CUR);
+    return 2;
+  }
+  
+  char *ctaille = entete.size;
+  int taille;
+  sscanf(ctaille,"%o",&taille);
+  int nb = (taille+512-1)/512;
+  
+  lseek(fichier,nb*512,SEEK_CUR);
+  return 0;
+}
+
+static int recherche3(int fichier, int option, char *nom){
+  //printf("Looping\n");
+  struct posix_header entete;
+  ssize_t lect = read(fichier,&entete,512);
+  
+  if(lect==0) return -1;
+  
+  if(lect<0){
+    perror("Echec de la lecture dans recherche");
+    return -1;
+  }
+
+  if(entete.name[0]=='\0'){
+    //printf("Je suis à la fin !!!");
+    return -1;
+  }
+  
+  if(check_checksum(&entete)==0){
+    //printf("entete.name : %s\n",entete.name); 
+    perror("Mauvaise entête. Je suis bloqué dans recherche !\n");
+    return -1;
+  }
+
+  if(strcmp(entete.name,nom)==0){
+    if(option == 1) lseek(fichier,-512,SEEK_CUR);
+    else debut3(fichier);
+    return 1;
+  }
+
+  lseek(fichier,-512,SEEK_CUR);
+
+  switch(suivant3(fichier)){
+  case 2:
+    printf("over\n");
+    return -1;
+  case 0:
+      return recherche3(fichier,option,nom);
+  default :
+    return -1;
+  }
+}
+
+static int rechercher3 (int fichier, int option, char *nom){
+  debut3(fichier);
+  return recherche3(fichier, option, nom);
+}
+
+static int fin_fic3(int fichier){
+  debut3(fichier);
+  while(1){
+    int x = suivant3(fichier);
+    if(x==2) break;
+    else if(x==-1) return -1;
+  }
+  return 0;
+}
 
 int contient(char *dossier,char *nom){
   DIR *d = opendir(dossier);
@@ -16,6 +102,7 @@ int contient(char *dossier,char *nom){
   }
   return 0;
 }
+
 
 int rmfichier_tar(char * path){
   int fd, n;
@@ -33,15 +120,25 @@ int rmfichier_tar(char * path){
     return 1;
   }
 
+  
+
   char * tarName = substringTar(); // recupere le nom du tar file
+  char * pathWithFile = createPathFile(pathRm);
+  
   fd = open(tarName, O_RDWR);
+  printf(" PATH WITH FILE : %s\n ",pathWithFile);
   if(fd < 0) {
     perror("open fichier tar");
     return -1;
   }
 
+  if(rechercher3(fd,0,pathWithFile)<=0){
+    return -1;
+  }
+  close(fd);
+
 // getcwd + tarpath + fichier a supprimer
-  char * pathWithFile = createPathFile(pathRm); 
+   
   rmOn = 1;
   if((n = rmftar(tarName, pathWithFile)) == -1) { //check si le fichier existe
     rmOn = 0;
@@ -54,7 +151,7 @@ int rmfichier_tar(char * path){
 }
 
 int rm_r_tar (char * path){
-  int n;
+  int fd,n;
   storePosition(); //store sa position
   char * pathCd = subWithoutRepo(path); // path cd
 
@@ -62,26 +159,43 @@ int rm_r_tar (char * path){
     return -1;
 
   char *pathRm = subWithRepo(path); // file a supprimer
+
+
+  int x = (strlen(pathRm)+5);
+  char pathRm2[x];
+  memset(pathRm2,'\0',x);
+  int i = 0;
+  while(i < strlen(pathRm) && pathRm[i]!='\0'){
+    pathRm2[i]=pathRm[i];
+    i++;
+  }
+  pathRm2[i]='/';
+
   
   if(TARPATH[0] == '\0') { // si t'es pas dans un tar alors tu appelles exec
     commandNoTar_option("rm","-r", pathRm); // appel fonctions avec exec
     restorePosition(); // restorePosition
     return 1;
   }
-  
-  int x = (strlen(pathRm)+1);
-  char pathRm2[x];
-  memset(pathRm2,'\0',x);
-  for(int i = 0; i < strlen(pathRm);i++){
-    pathRm2[i]=pathRm[i];
-  }
-  pathRm2[strlen(pathRm)]='/';
-  
+    
   char * tarName = substringTar(); // recupere le nom du tar file
 
   // getcwd + tarpath + fichier a supprimer
-  char * pathWithFile = createPathFile(pathRm); 
+  char * pathWithFile = createPathFile(pathRm2); 
   rmOn = 1;
+
+  fd = open(tarName, O_RDWR);
+  
+  if(fd < 0) {
+    perror("open fichier tar");
+    return -1;
+  }
+
+  if(rechercher3(fd,0,pathWithFile)<=0){
+    return -1;
+  }
+  close(fd);
+  
   if((n = checkEntete_r(tarName, pathWithFile)) == -1) { //check si le fichier existe
     rmOn = 0;
     restorePosition();
@@ -93,6 +207,8 @@ int rm_r_tar (char * path){
 
   /** **/
 }
+
+
 
 int rmTar(int nbOption, char ** command) {
   int i = 1;
