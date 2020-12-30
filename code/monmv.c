8664282d9854dd59247c4cd2ffbf4408c1e2e735
Julien BERSAUTER <julien.bersauter@orange.fr>
2020-12-30 12:58:14 +0100

    Corrections de la taille des tableaux et du nombre alloué à une variable.

--- code/monmv.c
+++ code/monmv.c
@@ -1,31 +1,45 @@
-#include "myCp.h"
-#include "monrm.h"
+#include <string.h>
+#include <stdlib.h>
+#include "monmv.h"
 
-char * monMv(int nbOption, char ** command, int dos){
+int monMv(int nbOption, char ** command, int dos){
   if (nbOption<3) return -1;
-  char ** c [nbOption-1];
-  if (dos == 1 && nbOption >= 4){
+  char * c [nbOption+1];
+  char * c1 [nbOption-1];
+  char * c2 [nbOption];
+  if (dos == 1){
     c[1] = malloc(strlen("-r")+1);
-    for(int i = 2; i<nbOption-1;i++){
-      c[i] = malloc(strlen(command[i])+1);
-      memset(c[i],'\0',strlen(command[i])+1);
-      strcpy(c[i],command[i]);
+    strcpy(c[1],"-r");
+
+    c2[1] = malloc(strlen("-r")+1);
+    strcpy(c2[1],"-r");
+    
+    for(int i = 1; i<nbOption;i++){
+      c[i+1] = malloc(strlen(command[i])+1);
+      memset(c[i+1],'\0',strlen(command[i])+1);
+      strcpy(c[i+1],command[i]);
+    }
+
+    for(int i = 1; i<nbOption-1; i++){
+      c2[i+1] = malloc(strlen(command[i])+1);
+      memset(c2[i+1],'\0',strlen(command[i])+1);
+      strcpy(c2[i+1],command[i]);
     }
     
-    for(int i = 2 ; i < nbOption-1; i++){
-      cpTar(nbOption,c);
-      rmTar(nbOption,c);
+    for(int i = 1 ; i < nbOption; i++){
+      cpTar(nbOption+1,c);
+      rmTar(nbOption,c2);
     }
   }
   else {
     for(int i = 1; i<nbOption -1 ;i++){
-      c[i] = malloc(strlen(command[i])+1);
-      memset(c[i],'\0',strlen(command[i])+1);
-      strcpy(c[i],command[i]);
+      c1[i] = malloc(strlen(command[i])+1);
+      memset(c1[i],'\0',strlen(command[i])+1);
+      strcpy(c1[i],command[i]);
     }
     for(int i = 1 ; i < nbOption-1;i++){
-      cpTar(nbOption,c);
-      rmTar(nbOption,c);
+      cpTar(nbOption,command);
+      rmTar(nbOption-1,c1);
     }
   }
   return 1;
